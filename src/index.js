ec31764fccaeaf84edfa204b1841d624f33961da
elina <papadelina145@gmail.com>
2023-04-27 13:02:08 +0300

    Change structure of files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import NavBar from './navbar';
 import Home from './home';
+import Menu from './menu';
 import './style.css';
 
 const content = document.getElementById('content');
 content.appendChild(NavBar.header);
 
-content.appendChild(Home.main);
+// content.appendChild(Home.main);
+content.appendChild(Menu.main);
+
+// NavBar.menuLink.addEventListener('click', () => {
+//     main.innerHTML = '';
+//     main.appendChild(Menu.menu);
+// });
